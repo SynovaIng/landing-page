cc571683f8e6da740889c6941828c0620f42366a
Johannsonmanson <91399377+JohannVasquez@users.noreply.github.com>
2026-03-03 00:43:26 -0300

    feat: create supabase shared client

--- src/config/container.ts
+++ src/config/container.ts
@@ -4,12 +4,14 @@ import { ContainerBuilder } from "diod";
 
 import { registerProjectsContainer } from "@/contexts/projects/app/projects.container";
 import { registerServicesContainer } from "@/contexts/services/app/services.container";
+import { registerSharedContainer } from "@/contexts/shared/app/shared.container";
 import { registerTestimonialsContainer } from "@/contexts/testimonials/app/testimonials.container";
 
 const builder = new ContainerBuilder();
 
 registerProjectsContainer(builder);
 registerServicesContainer(builder);
+registerSharedContainer(builder);
 registerTestimonialsContainer(builder);
 
 export const container = builder.build();
